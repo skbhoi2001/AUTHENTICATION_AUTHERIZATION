e9e578ec039bfe63a39f12e8332d1646f6d1fa70
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-12-13 00:09:43 +0530

    cors added

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 require('express-async-errors');
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const { errorHandler } = require('./middlewares/error');
 require('dotenv').config();
 require('./db');
 const userRouter = require('./routes/user');
+const { handleNotFound } = require('./utils/helper');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/user', userRouter);
+
+app.use('*', handleNotFound);
 app.use(errorHandler);
 
 // app.post("/sign-in",
