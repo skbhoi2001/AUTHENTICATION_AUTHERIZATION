624e351fbb6f062aa5be87872f58d457f3f91f83
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-12-18 14:24:24 +0530

    Backend initial done

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-require("express-async-errors");
-const express = require("express");
-const morgan = require("morgan");
-const { errorHandler } = require("./middlewares/error");
-const cors = require("cors");
-require("dotenv").config();
-require("./db");
-const userRouter = require("./routes/user");
-const actorRouter = require("./routes/actor");
-const { handleNotFound } = require("./utils/helper");
+require('express-async-errors');
+const express = require('express');
+const morgan = require('morgan');
+const { errorHandler } = require('./middlewares/error');
+const cors = require('cors');
+require('dotenv').config();
+require('./db');
+const userRouter = require('./routes/user');
+const actorRouter = require('./routes/actor');
+const movieRouter = require('./routes/movie');
+const reviewRouter = require('./routes/review');
+const { handleNotFound } = require('./utils/helper');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
-app.use("/api/user", userRouter);
-app.use("/api/actor", actorRouter);
-app.use("/*", handleNotFound);
+app.use(morgan('dev'));
+app.use('/api/user', userRouter);
+app.use('/api/actor', actorRouter);
+app.use('/api/movie', movieRouter);
+app.use('/api/review', reviewRouter);
+app.use('/*', handleNotFound);
 
 app.use(errorHandler);
 
@@ -31,5 +35,5 @@ app.use(errorHandler);
 //   });
 
 app.listen(8000, () => {
-  console.log("the port is listening on port 8000");
+  console.log('the port is listening on port 8000');
 });
