f8c5befd704e335b3004d16c4e1f9a259522a8d2
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-12-25 13:07:39 +0530

    Final test done

--- cloud/index.js
+++ cloud/index.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: 'soumyak-testing',
+  api_key: '662761355222978',
+  api_secret: 'raQrJOf7FHqXxIQRbN0ogtBT1k4',
   secure: true,
 });
 
